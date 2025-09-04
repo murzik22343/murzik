922f43e152e43079eca4283ac878d1f9f319b55f
murzik22343 <halilova889@gmail.com>
2025-09-04 23:57:55 +0600

    Update murzik lab.2.c

--- murzik/murzik lab.2.c
+++ murzik/murzik lab.2.c
@@ -6,12 +6,12 @@ int main() {
     setlocale(LC_ALL, "Rus");
     int test;
 
-    printf("\nÌåíþ:\n");
-    printf("[0] Âûõîä\n");
-    printf("[1] 0. Ïðèâåò Ìèð!\n");
-    printf("[2] 1.1 Òèïû äàííûõ\n");
-    printf("[3] 1.2 Îïåðàòîðû âåòâëåíèÿ\n");
-    printf("[4] 1.3 Öèêëû.\n");
+    printf("\nМеню:\n");
+    printf("[0] Выход\n");
+    printf("[1] 0. Привет Мир!\n");
+    printf("[2] 1.1 Типы данных\n");
+    printf("[3] 1.2 Операторы ветвления\n");
+    printf("[4] 1.3 Циклы.\n");
     scanf("%d", &test);
 
     switch (test) {
@@ -19,7 +19,7 @@ int main() {
         return 0;
 
     case 1:
-        printf("%s", "Ïðèâåò, Ìèð!");
+        printf("%s", "Привет, Мир!");
         break;
 
     case 2: {
@@ -34,29 +34,29 @@ int main() {
         float a9 = 1.0;
         double a10 = 1.0;
 
-        printf("char %c ðàâåí %zu áàéò\n", a1, sizeof(a1));
-        printf("unsigned char %c ðàâåí %zu áàéò\n", a2, sizeof(a2));
-        printf("short %hd ðàâåí  %zu\n", a3, sizeof(a3));
-        printf("unsighed short %hu ðàâåí %zu áàéò\n", a4, sizeof(a4));
+        printf("char %c равен %zu байт\n", a1, sizeof(a1));
+        printf("unsigned char %c равен %zu байт\n", a2, sizeof(a2));
+        printf("short %hd равен  %zu\n", a3, sizeof(a3));
+        printf("unsighed short %hu равен %zu байт\n", a4, sizeof(a4));
         printf("int %d %zu\n", a5, sizeof(a5));
-        printf("unsigned int %u ðàâåí %zu áàéò\n", a6, sizeof(a6));
-        printf("long %ld ðàâåí %zu áàéò\n", a7, sizeof(a7));
-        printf("long long %lld ðàâåí %zu áàéò\n", a8, sizeof(a8));
-        printf("float %f ðàâåí %zu áàéò\n", a9, sizeof(a9));
-        printf("double %lf ðàâåí %zu áàéò\n", a10, sizeof(a10));
+        printf("unsigned int %u равен %zu байт\n", a6, sizeof(a6));
+        printf("long %ld равен %zu байт\n", a7, sizeof(a7));
+        printf("long long %lld равен %zu байт\n", a8, sizeof(a8));
+        printf("float %f равен %zu байт\n", a9, sizeof(a9));
+        printf("double %lf равен %zu байт\n", a10, sizeof(a10));
         break;
     }
     case 3: {
         int c2;
-        printf("Ââåäèòå ãîä: ");
+        printf("Введите год: ");
         scanf("%d", &c2);
 
         if ((c2 % 400 == 0) || (c2 % 100 != 0 && c2 % 4 == 0)) {
-            printf("Âèñîêîñíûé");
+            printf("Високосный");
             break;
         }
         else {
-            printf("Íå âèñîêîñíûé");
+            printf("Не високосный");
             break;
         }
         return 0;
@@ -64,15 +64,15 @@ int main() {
     case 4: {
         int v33a, v33b;
 
-        printf("Ââåäèòå äâà íåîòðèöàòåëüíûõ öåëûõ ÷èñëà (a è b, ãäå a <= b): ");
+        printf("Введите два неотрицательных целых числа (a и b, где a <= b): ");
         scanf("%d %d", &v33a, &v33b);
 
         if (v33a > v33b) {
-            printf("Îøèáêà: a äîëæíî áûòü ìåíüøå èëè ðàâíî b!\n");
+            printf("Ошибка: a должно быть меньше или равно b!\n");
             return 1;
         }
 
-        printf("Êâàäðàòû ÷èñåë îò %d äî %d:\n", v33a, v33b);
+        printf("Квадраты чисел от %d до %d:\n", v33a, v33b);
 
         for (int n = v33a; n <= v33b; n++) {
             int square = 0;
